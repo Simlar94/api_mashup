4fe29f3f026b10a79ea87dfa4de5bac1d8e24e77
Simon Larsson <simon.larsson94@hotmail.com>
2018-01-12 21:13:08 +0100

    Added modules, promises.

---
+++ GUI/js/javascript.js
@@ -0,0 +1,127 @@
+$(document).ready(function () {
+
+    var APIUrl = "https://api.nasa.gov/planetary/apod?api_key=9B4Qpd8fTil57gHOvsehQ2dwXnoXva9f4q2FsGUn"
+    var DBUrl = "http://localhost:3000/testdb"; //On Pi: 192.168.48.248:3000/testdb
+    var APIDataExists = false;
+    var DBDataExists = false;
+
+
+    $.getJSON(APIUrl, function (data) {
+
+        var title = data.title;
+        var explanation = data.explanation;
+        var copyright = data.copyright;
+        var date = data.date;
+        var img = data.hdurl;
+
+        var tr = $("<tr/>");
+        tr.append("<td>" + title + "</td>");
+        tr.append("<td><img src='" + img + "' style='width: 100%'></td>");
+        tr.append("<td>" + explanation + "</td>");
+        tr.append("<td>" + copyright + "</td>");
+        tr.append("<td>" + date + "</td>");
+        tr.append("<td><form method='POST' action='" + DBUrl + "'><input type='hidden' name='title' value='" + title + "'/><input type='hidden' name='explanation' value='" + explanation + "'/><input type='hidden' name='copyright' value='" + copyright + "'/><input type='hidden' name='img' value='" + img + "'/><input type='hidden' name='date' value='" + date + "'/><input class='add' type='submit' value='+'/></form></td>");
+        $(".table-left3").append(tr);
+
+    });
+
+
+    //Handle JSON-data from my own database.
+    $.getJSON(DBUrl, function (data) {
+
+
+        for (var i = 0; i < data.length; i++) {
+            var title = data[i].title;
+            var explanation = data[i].explanation;
+            var copyright = data[i].copyright;
+            var date = data[i].date;
+            var img = data[i].img;
+            var id = data[i]._id;
+
+            var tr = $("<tr/>");
+            tr.append("<td class='id' style='display:none'>" + id + "</td>");
+            tr.append("<td>" + title + "</td>");
+            tr.append("<td><img src='" + img + "' style='width: 100%'></td>");
+            tr.append("<td>" + explanation + "</td>");
+            tr.append("<td>" + copyright + "</td>");
+            tr.append("<td>" + date + "</td>");
+            tr.append("<td width='10%'><button type='button' class='del'>" + "x" + "</button></td>");
+            $(".table-right").append(tr);
+
+            $(".del").click(function () {
+                // Get current row's ID.
+                var row = $(this).closest("tr"); // Find the row.
+                var id = row.find(".id").text(); // Find the row's content (ID).
+
+                // Configure and execute DELETE request.
+                $.ajax({
+                    type: "DELETE",
+                    url: "http://localhost:3000/testdb/" + id
+                }).then(function () {
+                    window.location.reload(); // Reload window after deletion.
+                });
+            });
+        };
+    });
+
+    function APITableCheck() {
+        //Checks if table rows is more than 1.
+        if ($(".table-left3 tr").length > 1) {
+            //If rows is more than 1, make boolean true.
+            APIDataExists = true;
+        };
+        //Configures a promise.
+        var tablePromise = new Promise(function (resolve, reject) {
+            //If boolean is true, resolve with string.
+            if (APIDataExists) {
+                var exists = "API-data exists.";
+                resolve(exists); // fulfilled
+            } else {
+                //If boolean is false, reject with string.
+                var reason = "API-data failed to load. try again later.";
+                reject(reason); // reject
+            }
+
+        });
+        //Execute promise. 
+        tablePromise.then(function(fromResolve) {
+            console.log(fromResolve);
+        }).catch(function(fromReject) {
+            alert(fromReject);
+        }); 
+
+    };
+
+
+    function DBTableCheck() {
+        //Checks if table rows is more than 1.
+        if ($(".table-right tr").length > 1) {
+            //If rows is more than 1, make boolean true.
+            DBDataExists = true;
+        };
+        //Configures a promise.
+        var tablePromise = new Promise(function (resolve, reject) {
+            //If boolean is true, resolve with string.
+            if (DBDataExists) {
+                var exists = "Database-data exists.";
+                resolve(exists); // fulfilled
+            } else {
+                //If boolean is false, reject with string.
+                var reason = "Database-data failed to load. try again later.";
+                reject(reason); // reject
+            }
+
+        });
+        //Execute promise. 
+        tablePromise.then(function(fromResolve) {
+            console.log(fromResolve);
+        }).catch(function(fromReject) {
+            alert(fromReject);
+        }); 
+
+    };
+    //Call after 2 seconds to let tables find data.
+    setTimeout(APITableCheck, 2000);
+    setTimeout(DBTableCheck, 2000);
+
+});
---
+++ server.js
@@ -0,0 +1,83 @@
+//Imported modules.
+var express = require('express');
+var pouchDB = require('pouchdb');
+var bodyParser = require('body-parser');
+var serverInfo = require('./custom_modules/server-info.js');
+
+var app = express();
+
+//Global variables.
+var dbName = "testdb";
+var database = new pouchDB("http://127.0.0.1:5984/" + dbName);
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+
+// Allow CORS(Cross Origin Resource Sharing). Decides who gets to access your server and what they are able to do.
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "*, DELETE");
+    next();
+});
+
+
+//GET-method: Get items from my database.
+app.get("/" + dbName, function (req, res) {
+    database.allDocs({
+        include_docs: true
+    }).then(function (result) {
+        res.send(result.rows.map(function (item) {
+            return item.doc;
+        }));
+    }, function (error) {
+        res.status(400).send(error);
+    });
+});
+
+
+//POST-method: Post an item to my database.
+app.post("/" + dbName, function (req, res) {
+    database.post(req.body).then(function (result) {
+        console.log("Posted at: " + dateAndTime());
+
+        res.redirect("back"); // Redirects user to the previous page.
+    }, function (error) {
+        res.status(400).send(error);
+    });
+});
+
+
+// DELETE specific ID from database.
+app.delete("/" + dbName + "/:id", function (req, res) {
+    database.get(req.params.id).then(function (result) {
+        database.remove(result);
+    }).then(function (result) {
+        console.log("Deleted at: " + dateAndTime());
+
+        res.send(result);
+    });
+
+});
+
+/*
+app.put("/" + dbName + "/:id", function (req, res){
+   database.get(req.params.id).then(function (result){
+      result.name = req.body.name;
+       database.put(result);
+       res.send(result);
+   }) 
+});
+*/
+app.listen(3000, function (error) {
+    if (!error) {
+        console.log("Server started on port 3000.")
+        serverInfo();
+    } else {
+        console.log("Error. Something went wrong.")
+    }
+});
